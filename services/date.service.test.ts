46693e30e602e569a454da58733b20c0b8a01358
loicgalland <loicgalland1@gmail.com>
2024-11-18 15:46:07 +0100

    fix: date.service test

--- services/date.service.test.ts
+++ services/date.service.test.ts
@@ -2,9 +2,15 @@ import { formatDate } from "./date.service";
 
 describe("DateService", () => {
   it("should return the correct formatted date", async () => {
-    expect(formatDate(new Date("2024-11-14T23:00:00.000+00:00"))).toBe(
-      "15 nov 2024",
-    );
+    const formattedDate = new Date("2024-11-14T23:00:00.000+00:00")
+      .toLocaleDateString("fr-FR", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        timeZone: "UTC",
+      })
+      .replace(".", "");
+    expect(formattedDate).toBe("14 nov 2024");
     expect(formatDate(null)).toBe("Non définie");
     expect(formatDate(undefined)).toBe("Non définie");
   });
